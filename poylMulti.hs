882f6332c726934e4dd3cb843e2fc84093b2ec6e
Gabriel Sailer <s_sailer@i08pc48.atis-stud.uni-karlsruhe.de>
2017-11-13 17:03:58 +0100

    started out but wasn't quite sure how to construct

---
+++ poylMulti.hs
@@ -0,0 +1,6 @@
+-- Polynomial multiplication
+poly1=[0,6,2,3,1]
+poly2=[0,0,1]
+-- desired result: [0,0,0,6,2,3,1,0]
+-- multiply each element in poly1 with each el in poly2 and put in in pos [pos(poly1[el])+pos(poly2[el]))]
+result=
