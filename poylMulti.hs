6dd6102e32a231562b7c7ad0961aeac9021de70e
Gabriel Sailer <gabriel.sailer@riseup.net>
2017-11-16 19:00:16 +0100

    working solution

--- poylMulti.hs
+++ poylMulti.hs
@@ -1,10 +1,16 @@
 -- Polynomial multiplication
-poly1=[0,6,2,3,1]
-poly2=[0,0,1]
--- desired result: [0,0,0,6,2,3,1,0]
--- multiply each element in poly1 with each el in poly2 and put in in pos [pos(poly1[el])+pos(poly2[el]))]
+-- polynomial representation in tuples of (coefficient, exponent)
+poly1=[(6,1),(2,2),(3,3),(1,4)]
+poly2=[(1,2)]
+-- filters all elem of a list of tuples at the snd place for a pattern 
+maphelp [] = []
+maphelp [x] = []
+maphelp l = l
+maptuple k (x:xs) =  maphelp (filter (\x -> k == snd x) (x:xs))
+-- multiply each coefficient in poly1 with each coefficient in poly2 and add exponents
+mult :: [(Integer,Integer)] -> [(Integer,Integer)] -> [(Integer,Integer)]
+mult  [] [] = []
+mult lst1 lst2 = head(map (\a -> (map (\x -> ((fst x)*(fst a),(snd x)+(snd a))) lst1)) lst2)
 
-pmultify :: [Int] -> [Int] -> [Int]
-pmultify [0] [0] = [0]
-
-main = mapM_ print (pmultify [0] [0])
+-- desired result: [(6,3),(2,4),(3,5),(1,6)]
+-- main = mapM_ print (pmultify poly1 poly2)
