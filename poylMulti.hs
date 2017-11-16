876c041ae14ca9292d6ef5a547c7b054904b3f90
Gabriel Sailer <gabriel.sailer@riseup.net>
2017-11-16 12:34:21 +0100

    working compilation

--- poylMulti.hs
+++ poylMulti.hs
@@ -3,4 +3,8 @@ poly1=[0,6,2,3,1]
 poly2=[0,0,1]
 -- desired result: [0,0,0,6,2,3,1,0]
 -- multiply each element in poly1 with each el in poly2 and put in in pos [pos(poly1[el])+pos(poly2[el]))]
-result=
+
+pmultify :: [Int] -> [Int] -> [Int]
+pmultify [0] [0] = [0]
+
+main = mapM_ print (pmultify [0] [0])
