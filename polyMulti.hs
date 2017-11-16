e4a61f06bd2b945cbcb1d7cda27f2efbb3e557e6
Gabriel Sailer <gabriel.sailer@riseup.net>
2017-11-17 00:17:29 +0100

    multiplies polynoms

--- polyMulti.hs
+++ polyMulti.hs
@@ -1,16 +1,14 @@
 -- Polynomial multiplication
--- polynomial representation in tuples of (coefficient, exponent)
+-- | polynomial representation in list of tuples: (coefficient, exponent)
+import Data.List
+import Data.Ord
+-- testdata
 poly1=[(6,1),(2,2),(3,3),(1,4)]
-poly2=[(1,2)]
--- filters all elem of a list of tuples at the snd place for a pattern 
-maphelp [] = []
-maphelp [x] = []
-maphelp l = l
-maptuple k (x:xs) =  maphelp (filter (\x -> k == snd x) (x:xs))
+poly2=[(1,2),(2,4)]
+expected=[(6,3),(2,4),(15,5),(5,6),(6,7),(2,8)]
+-- helper funcs
+sumtupfst lst = sum (map (\x -> fst x) lst)
+-- | multPoly takes two polynoms and returns the product.
 -- multiply each coefficient in poly1 with each coefficient in poly2 and add exponents
-mult :: [(Integer,Integer)] -> [(Integer,Integer)] -> [(Integer,Integer)]
-mult  [] [] = []
-mult lst1 lst2 = head(map (\a -> (map (\x -> ((fst x)*(fst a),(snd x)+(snd a))) lst1)) lst2)
-
--- desired result: [(6,3),(2,4),(3,5),(1,6)]
--- main = mapM_ print (pmultify poly1 poly2)
+multPoly :: [(Integer,Integer)] -> [(Integer,Integer)] -> [(Integer,Integer)]
+multPoly lst1 lst2 = nubBy (\f g -> f == g) (concat (map (\d -> map (\e -> (sumtupfst d, snd e)) d) (groupBy (\b c -> (snd b) == (snd c)) (sortBy (comparing snd) (concat (foldr (\a -> ((foldr (\x -> (((fst x)*(fst a),(snd x)+(snd a)) :)) [] lst1) : )) [] lst2))))))
